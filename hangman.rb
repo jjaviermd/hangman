dec06d49cd9800582e5aeb72df7e53cfda4c04fe
jjav <razornice@gmail.com>
2022-10-12 12:31:05 -0300

    basic variables and constants added

--- hangman.rb
+++ hangman.rb
@@ -1,3 +1,20 @@
 SOURCE = File.readlines("google-10000-english-no-swears.txt").
-  select{ |line| line.length.between?(6,12)}
-  puts SOURCE
+  select { |line| line.length.between?(6,12)}
+
+counter = 10
+
+secret_world = SOURCE.sample.chomp
+
+guess_line = String.new("_") * secret_world.length
+
+def guess_letter
+  if counter == 10
+    puts "wich letter will be your first guess?"
+    input = gets.chomp.to_upcase
+  else
+   puts "Next letter?"
+   input = gets.chomp.to_upcase
+  end
+end
+
+guess_letter
